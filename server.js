2db3d2ee73383c2ba08c08c047aa525242d84d7e
Annaruth McBride <armcbride17@gmail.com>
2020-06-10 13:04:14 -0500

    adjustments

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budgeter", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
