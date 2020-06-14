380369e13111fdd2d11dc10e789bf393fcaab1f3
Annaruth McBride <armcbride17@gmail.com>
2020-06-14 14:53:31 -0500

    adjustment

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,6 @@ const FILES_TO_CACHE = [
     '/manifest.webmanifest',
     '/assets/css/style.css',
     '/assets/js/index.js',
-    '/assets/js/db.js',
     '/assets/images/icons/icon-144x144.png',
     '/assets/images/icons/icon-192x169.png',
     '/assets/images/icons/icon-512x452.png',
